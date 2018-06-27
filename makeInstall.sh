f1fadb3f9e887a87215fa6172a93b0ef6b59ca33
Samuele Carcagno <sam.carcagno@gmail.com>
2018-06-27 18:17:17 +0100

    Fix bug in wGeoMean

--- makeInstall.sh
+++ makeInstall.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 R CMD build psychophysics/
-R CMD INSTALL -l ~/.R_library psychophysics_1.0.2.tar.gz
+R CMD INSTALL -l ~/.R_library psychophysics_1.0.3.tar.gz
 rm psychophysics.pdf
 R CMD Rd2pdf psychophysics/
 
